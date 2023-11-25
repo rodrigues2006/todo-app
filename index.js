98f3f59b098640e79ca209774b16dc8284b8da4a
Joao Victor <joaovictor.zuca12@gmail.com>
2023-11-25 19:01:20 -0300

    iniciando o filtro de tarefas ativas

--- index.js
+++ index.js
@@ -70,6 +70,9 @@ app.post('/criar',(requisicao, resposta)=>{
   })
 })
 
+app.get('/ativas',(requisicao,resposta)=>{
+  
+})
 
 app.get('/', (requisicao, resposta)=>{
   const sql ='SELECT * FROM tarefas'
