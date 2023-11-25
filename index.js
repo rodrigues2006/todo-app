9f7854a30fe228f012029367c5d2fcae5e149a60
Joao Victor <joaovictor.zuca12@gmail.com>
2023-11-25 17:20:16 -0300

    salvando uma tarefa no banco de dados

--- index.js
+++ index.js
@@ -8,6 +8,33 @@ app.engine('handlebars',ephbs.engine())
 app.set('view engine','handlebars')
 
 app.use(express.static('public'))
+//converter dados do formulário em objeto javascript
+app.use(express.urlencoded({
+  extended: true
+}))
+
+app.use(express.json())
+
+//rotas
+
+app.post('/criar',(requisicao, resposta)=>{
+  const descricao = requisicao.body.descricao
+  const completa = 0
+
+  const sql = `
+   INSERT INTO tarefas (descricao, completa)
+   VALUES ('${descricao}','${completa}')
+  `
+
+  conexao.query(sql, (erro)=>{
+    if (erro){
+      return console.log(erro)
+    }
+
+    resposta.redirect('/')
+  })
+})
+
 
 app.get('/', (requisicao, resposta)=>{
     resposta.render('home')
