d6988541470fec7f6261e5542ac5b5a7694eb05d
Joao Victor <joaovictor.zuca12@gmail.com>
2023-11-25 18:33:54 -0300

    completar tarefa no banco de dados

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,16 @@
+function completarTarefa(id){
+    fetch("http://localhost:3000/completar",{
+        method:"POST",
+        headers:{
+            'content-Type':'application/json'
+        },
+
+        body: JSON.stringify({id})
+    })
+
+    window.location.reload()
+}
+
 function alterarTema() {
      const tema = localStorage.getItem("tema")
      const body = document.querySelector("body")
